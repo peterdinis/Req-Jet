326f1477098b681c0528b844832be6c1892e7099
peterdinis <petter1@pobox.sk>
2025-10-05 14:30:11 +0200

    feat: Initial migration

---
+++ supabase/client.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from './types';
+import { env } from '@/env';
+
+export const supabase = createClient<Database>(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
+  auth: {
+    storage: localStorage,
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+});
